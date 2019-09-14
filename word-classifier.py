469ba5bb4dadaeaf897521669dad661f93843299
Tullio Facchinetti <tullio.facchinetti@unipv.it>
2019-09-14 17:43:53 +0200

    Parametrized the keys.
    Some random touch ups.

--- word-classifier.py
+++ word-classifier.py
@@ -3,9 +3,33 @@ import csv
 import curses
 import logging
 import argparse
+import operator
 
+
+# List of class names
+KEYWORD = 'keyword'
+NOISE = 'noise'
+RELEVANT = 'relevant'
+NOTRELEVANT = 'not-relevant'
+
+keys = {
+    KEYWORD: 'k',
+    NOISE: 'n',
+    RELEVANT:'r',
+    NOTRELEVANT: 'x'
+}
+
+
+# FIXME: the key2class dict shall be obtained by "reversing" the keys dict
+key2class = {
+    'k': KEYWORD,
+    'n': NOISE,
+    'r': RELEVANT,
+    'x': NOTRELEVANT
+}
 
 def init_argparser():
+    """Initialize the command line parser."""
     parser = argparse.ArgumentParser()
     parser.add_argument('datafile', action="store", type=str,
         help="input CSV data file")
@@ -34,10 +58,11 @@ def write_words(outfile):
         writer = csv.writer(out, delimiter=',', quotechar='"', quoting=csv.QUOTE_MINIMAL)
         writer.writerow(header)
         for w in words:
-            writer.writerow(w[:3])
+            writer.writerow(w[:4])
 
 
 class Win(object):
+    """Contains the list of lines to display."""
     def __init__(self, key, title='', rows=3, cols=30, y=0, x=0):
         self.key = key
         self.title = title
@@ -48,13 +73,13 @@ class Win(object):
         self.win_handler = curses.newwin(self.rows, self.cols, self.y, self.x)
         self.win_handler.border()
         self.win_handler.refresh()
-        self.words = []
+        self.lines = []
 
-    def display_words(self, rev=True, highlight_word=None):
+    def display_lines(self, rev=True, highlight_word=None):
         if rev:
-            word_list = reversed(self.words)
+            word_list = reversed(self.lines)
         else:
-            word_list = self.words
+            word_list = self.lines
         i = 0
         for w in word_list:
             trunc_w = w[:self.cols - 2]
@@ -76,12 +101,13 @@ class Win(object):
         self.win_handler.border()
         self.win_handler.refresh()
 
-    def assign_words(self, words):
-        self.words = [w[0] for w in words if w[2] == self.key]
-        #print(self.words)
+    def assign_lines(self, lines):
+        self.lines = [w[0] for w in lines if w[2] == self.key]
+        #print(self.lines)
 
 
 def avg_or_zero(num, den):
+    """Safely calculatess an average, returning 0 if no elements are present."""
     if den > 0:
         avg = 100 * num / den
     else:
@@ -127,10 +153,11 @@ def return_related_items(words, key):
     return containing, not_containing
 
 
-def mark_word(words, word, marker):
+def mark_word(words, word, marker, order):
     for w in words:
         if w[0] == word:
             w[2] = marker
+            w[3] = order
             break
     return words
 
@@ -155,61 +182,82 @@ def main(args, words, datafile):
 
     # define windows
     windows = {
-        'k': Win('k', title='Keywords', rows=12, cols=win_width, y=0, x=0),
-        'n': Win('n', title='Noise', rows=12, cols=win_width, y=12, x=0),
-        'x': Win('x', title='Not-relevant', rows=12, cols=win_width, y=24, x=0)
+        KEYWORD: Win(KEYWORD, title='Keywords', rows=12, cols=win_width, y=0, x=0),
+        NOISE: Win(NOISE, title='Noise', rows=12, cols=win_width, y=12, x=0),
+        NOTRELEVANT: Win(NOTRELEVANT, title='Not-relevant', rows=12, cols=win_width, y=24, x=0)
     }
     curses.ungetch(' ')
     c = stdscr.getch()
     for win in windows:
-        windows[win].assign_words(words)
-        windows[win].display_words()
+        windows[win].assign_lines(words)
+        windows[win].display_lines()
     words_window = Win(None, rows=27, cols=win_width, y=9, x=win_width)
     stats_window = Win(None, rows=9, cols=win_width, y=0, x=win_width)
-    stats_window.words = get_stats_strings(words)
-    stats_window.display_words(rev=False)
+    stats_window.lines = get_stats_strings(words)
+    stats_window.display_lines(rev=False)
 
     related_items_count = 0
-    words_window.words = [w[0] for w in words if w[2] == '']
+    words_window.lines = [w[0] for w in words if w[2] == '']
     sort_word_key = None
+    orders = [int(w[3]) for w in words if w[3] != '']
+    if len(orders) == 0:
+        order = 0
+    else:
+        order = max(orders)
     while True:
-        evaluated_word = words_window.words[0]
-        words_window.display_words(rev=False, highlight_word=sort_word_key)
+        evaluated_word = words_window.lines[0]
+        words_window.display_lines(rev=False, highlight_word=sort_word_key)
         c = stdscr.getch()
-        if c in [ord('k'), ord('x')]:
-            words = mark_word(words, evaluated_word, chr(c))
-            win = windows[chr(c)]
-            win.words.append(evaluated_word)
-            words_window.words = words_window.words[1:]
-            win.display_words()
+        if c in [ord(keys[KEYWORD]), ord(keys[NOTRELEVANT])]:
+            words = mark_word(words, evaluated_word, chr(c), order)
+            order += 1
+            win = windows[key2class[chr(c)]]
+            win.lines.append(evaluated_word)
+            words_window.lines = words_window.lines[1:]
+            win.display_lines()
             related_items_count -= 1
-        elif c == ord('n'):
-            words = mark_word(words, evaluated_word, chr(c))
-            win = windows[chr(c)]
-            win.words.append(evaluated_word)
-            win.display_words()
+        elif c == ord(keys[NOISE]):
+            words = mark_word(words, evaluated_word, chr(c), order)
+            order += 1
+            win = windows[key2class[chr(c)]]
+            win.lines.append(evaluated_word)
+            win.display_lines()
             if related_items_count <= 0:
                 sort_word_key = evaluated_word
             containing, not_containing = return_related_items(words, sort_word_key)
             if related_items_count <= 0:
                 related_items_count = len(containing) + 1
             #logging.debug("related_items_count: {}".format(related_items_count))
-            words_window.words = containing
-            words_window.words.extend(not_containing)
-            #logging.debug("words_window.words: {}".format(words_window.words))
-            words_window.display_words(rev=False, highlight_word=sort_word_key)
+            words_window.lines = containing
+            words_window.lines.extend(not_containing)
+            #logging.debug("words_window.lines: {}".format(words_window.lines))
+            words_window.display_lines(rev=False, highlight_word=sort_word_key)
             related_items_count -= 1
         elif c == ord('p'):
-            words = mark_word(words, evaluated_word, chr(c))
-            words_window.words = words_window.words[1:]
+            words = mark_word(words, evaluated_word, chr(c), order)
+            order += 1
+            words_window.lines = words_window.lines[1:]
             related_items_count -= 1
         elif c == ord('w'):
             write_words(datafile)
+        elif c == ord('u'):
+            orders = [int(w[3]) for w in words if w[3] != '']
+            if len(orders) == 0:
+                continue
+            else:
+                max_index, max_value = max(enumerate(orders), key=operator.itemgetter(1))
+            w = words[max_index][0]
+            logging.debug("{} {} {}".format(max_index, max_value, w))
+            words = mark_word(words, w, '', '')
+            order -= 1
+            rwl = [w]
+            rwl.extend(words_window.lines)
+            words_window.lines = rwl
         elif c == ord('q'):
             break
-        stats_window.words = get_stats_strings(words)
-        stats_window.words.append('Related:      {:7}'.format(related_items_count if related_items_count >= 0 else 0))
-        stats_window.display_words(rev=False)
+        stats_window.lines = get_stats_strings(words)
+        stats_window.lines.append('Related:      {:7}'.format(related_items_count if related_items_count >= 0 else 0))
+        stats_window.display_lines(rev=False)
 
 
 if __name__ == "__main__":
