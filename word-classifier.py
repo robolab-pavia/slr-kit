db7d379de0f8328cc46fd6ed3a0869443386d82b
Guido Benetti <guido.benetti01@ateneopv.it>
2019-10-07 19:36:34 +0200

    Define a new color pair for the last marked word

--- word-classifier.py
+++ word-classifier.py
@@ -298,6 +298,7 @@ def init_curses():
     curses.start_color()
     curses.use_default_colors()
     curses.init_pair(1, curses.COLOR_RED, curses.COLOR_BLACK)
+    curses.init_pair(2, curses.COLOR_YELLOW, curses.COLOR_BLACK)
 
     return stdscr
 
