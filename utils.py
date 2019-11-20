96350f84d21567909f429998b2476ac5617c8c85
Guido Benetti <guido.benetti01@ateneopv.it>
2019-11-20 18:42:11 +0100

    Add utility function to find where a substring is in a string

--- utils.py
+++ utils.py
@@ -23,3 +23,44 @@ def setup_logger(name, log_file, formatter=logging.Formatter('%(asctime)s %(leve
     logger.setLevel(level)
     logger.addHandler(handler)
     return logger
+
+
+def substring_index(haystack, needle):
+    """
+    Generator function that give the slices of haystack where needle is found
+
+    needle is considered found if it is between two word boundaries.
+    Example:
+    haystack = 'the rate monotonic scheduling algorithm to meet the deadlines'
+    needle = 'to'
+    the generator yields only the 'to' between 'algorithm' and 'meet' not the
+    'to' inside 'monoTOnic'.
+    The values yielded are in the form of a tuple (begin, end) with
+    haystack[begin:end] == needle
+    If needle is not found the generator raises immediately StopIteration
+    :param haystack: the string to examine
+    :type haystack: str
+    :param needle: the string to search
+    :type needle: str
+    :return: a generator that yields the slice indexes where needle is found
+    """
+    if needle == '':
+        return
+
+    if haystack == needle:
+        yield (0, len(haystack))
+
+    start = 0
+    idx = haystack.find(needle, start)
+    while idx >= 0:
+        start = idx + len(needle)
+        if idx == 0:
+            if haystack[len(needle)] == ' ':
+                yield (0, len(needle))
+        elif haystack[idx - 1] == ' ':
+            if idx + len(needle) == len(haystack):
+                yield (idx, len(haystack))
+            elif haystack[idx + len(needle)] == ' ':
+                yield (idx, idx + len(needle))
+
+        idx = haystack.find(needle, start)
