6db2ddafcd50d1bd0347bb8912adc8c2574239f3
Marco Lagalla <marcolagalla@gmail.com>
2020-02-21 11:50:40 +0100

    Removed unused comments and imports

--- hierarchical-clustering.py
+++ hierarchical-clustering.py
@@ -1,14 +1,14 @@
-import sys
-import matplotlib.pyplot as plt
-import pandas as pd
 import argparse
 import logging
+import sys
+
+import pandas as pd
+from sklearn.cluster import AgglomerativeClustering
+
 from utils import (
     load_df,
     setup_logger
 )
-from scipy.cluster.hierarchy import dendrogram, ward
-from sklearn.cluster import AgglomerativeClustering
 
 default_n_clusters = 5
 
@@ -41,11 +41,6 @@ def main():
 
     docs = load_df(args.datafile, required_columns=['id', 'title'])
 
-    # since abstract isn't available for some entries we have to filter them
-    # because they have been skipped in processing phases and are not present
-    # in the dataset used in clustering
-
-
     debug_logger.debug('[hierarchical clustering] Computing clusters')
 
     num_clusters = default_n_clusters
@@ -56,36 +51,11 @@ def main():
     ward_cl.fit(dist_matrix)
     labels = ward_cl.labels_
 
-    ''' different implementation
-    linkage_matrix = ward(dist_matrix)  # define the linkage_matrix using ward clustering
-    
-    # obtain clusters from the linkage matrix setting the max number
-    # of clusters to obtain equal to 'num_clusters'
-    labels = fcluster(linkage_matrix, num_clusters, criterion='maxclust')
-    '''
-
-    ''' comment to plot dendrogram
-    linkage_matrix = ward(dist_matrix)  # define the linkage_matrix using ward clustering
-    
-    fig, ax = plt.subplots(figsize=(15, 20))  # set size
-    # TODO: indexes are used as temporary labels --> document titles?
-    ax = dendrogram(linkage_matrix, orientation="left", labels=terms['term'].values)
-    plt.axvline(x=20, color='r', linestyle='--')
-    plt.tick_params(
-        axis='x',  # changes apply to the x-axis
-        which='both',  # both major and minor ticks are affected
-        bottom='off',  # ticks along the bottom edge are off
-        top='off',  # ticks along the top edge are off
-        labelbottom='off')
-
-    plt.tight_layout()  # show plot with tight layout
-
-    #uncomment below to save graph to file
-    plt.savefig('ward_clusters.png', dpi=300)
-    plt.show()
-    plt.close()
-    
-    #'''
+    # linkage_matrix = ward(dist_matrix)  # define the linkage_matrix using ward clustering
+    #
+    # # obtain clusters from the linkage matrix setting the max number
+    # # of clusters to obtain equal to 'num_clusters'
+    # labels = fcluster(linkage_matrix, num_clusters, criterion='maxclust')
 
     df = pd.DataFrame(dict(title=docs['title'], label=labels), index=dist_matrix.index)
 
