e9eaef31e9dbe4ed66d21f75fe6019bb3b20c762
Simone <simone.tartarotti01@universitadipavia.it>
2021-03-16 16:13:31 +0100

    Code cleaned and grouped in functions; added argparse

--- report_year.py
+++ report_year.py
@@ -1,49 +1,68 @@
 import json
 import csv
 import collections
+import argparse
 from matplotlib import pyplot as plt
 
-year = 0
-paper_id = 0
-topic_dic = collections.defaultdict(dict)
 
-plt.style.use("fivethirtyeight")
-
-with open("energy1/energy_docs-topics.json") as file:
-    topics_data = json.load(file)
-
-#print(type(topics_data[0]))
-
-papers_data = list(csv.reader(open("energy1/energy_preproc.csv", encoding="utf8"), delimiter="\t"))
-
-papers_dic = dict()
-
-for paper in papers_data[1:]:
-    key = int(paper[0])
-    data = int(paper[2])
-    papers_dic[key] = data
-
-for entry in topics_data:
-    paper_id = entry["id"]
-    year = papers_dic.get(paper_id)
-    for topic in entry["topics"]:
-        topic_dic[topic][year] = topic_dic[topic].get(year, 0) + 1
-
-for dic in topic_dic:
-
-    sorted_dic = sorted(topic_dic[dic].items())
-    x, y = zip(*sorted_dic)
-
-    plt.grid(True)
-    plt.plot(x, y)
-    plt.title("topic number: "+dic)
-    plt.xlabel("Year")
-    plt.ylabel("# of papers")
-    plt.tight_layout()
-    plt.show()
+def init_argparser():
+    parser = argparse.ArgumentParser()
+    # arguments needed
+    parser.add_argument("json_file", type=str, help="the json file with the list of the papers")
+    parser.add_argument("csv_file", type=str, help="the csv file with the papers data")
+    return parser
 
 
+def file_reader(json_path, csv_path):
+    with open(json_path) as file:
+        topics_data = json.load(file)
+
+    papers = csv.reader(open(csv_path, encoding="utf8"), delimiter="\t")
+    return topics_data, papers
 
 
+def dict_builder(topics, papers):
+    papers_data = list(papers)
+    papers_dic = dict()
+    topic_dic = collections.defaultdict(dict)
+    for paper in papers_data[1:]:
+        key = int(paper[0])
+        data = int(paper[2])
+        papers_dic[key] = data
 
+    for entry in topics:
+        paper_id = entry["id"]
+        year = papers_dic.get(paper_id)
+        for topic in entry["topics"]:
+            topic_dic[topic][year] = topic_dic[topic].get(year, 0) + 1
+
+    return topic_dic
+
+
+def plotter(topic_dic):
+    plt.style.use("fivethirtyeight")
+    for dic in topic_dic:
+        sorted_dic = sorted(topic_dic[dic].items())
+        x, y = zip(*sorted_dic)
+
+        plt.grid(True)
+        plt.plot(x, y)
+        plt.title("topic number: " + dic)
+        plt.xlabel("Year")
+        plt.ylabel("# of papers")
+        plt.tight_layout()
+        plt.show()
+
+
+def main():
+    parser = init_argparser()
+    args = parser.parse_args()
+
+    topics_data, papers = file_reader(args.json_file, args.csv_file)
+    topics_dict = dict_builder(topics_data, papers)
+    plotter(topics_dict)
+
+
+if __name__ == "__main__":
+    main()
 
