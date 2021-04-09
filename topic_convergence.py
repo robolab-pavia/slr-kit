c3f54f223eee2fa12b6863b369319f7727890141
Simone <simone.tartarotti01@universitadipavia.it>
2021-04-09 14:22:41 +0200

    added function list_sorter with progressbar

--- topic_convergence.py
+++ topic_convergence.py
@@ -1,43 +1,60 @@
 import argparse
 import terms
 import copy
+from tqdm import tqdm
 
 def init_argparser():
     parser = argparse.ArgumentParser()
     return parser
 
 
-def list_sorter(tsv_path):
-    sorter = terms.TermList()
-    sorter.from_tsv(tsv_path)
+def list_cleaner(tsv_path):
+    terms_list = terms.TermList()
+    terms_list.from_tsv(tsv_path)
     relevant_list = terms.TermList([])
-    for item in list(sorter.items):
+    print("Starting list cleaning")
+    for item in list(terms_list.items):
         if item.order < 0:
-            sorter.items.remove(item)
+            terms_list.items.remove(item)
         else:
             if item.label == terms.Label.RELEVANT or item.label == terms.Label.KEYWORD:
                 copy_item = copy.deepcopy(item)
                 relevant_list.items.append(copy_item)
-            sorter.get(item.string).order = -1
-            sorter.get(item.string).label = terms.Label.NONE
+            terms_list.get(item.string).order = -1
+            terms_list.get(item.string).label = terms.Label.NONE
 
+    print("List has been cleaned")
+
+    return terms_list, relevant_list
+
+
+def list_sorter(terms_list):
     multigram_index = 0
+    print("Starting list sorting")
 
-    for term in sorter.items:
+    for term in terms_list.items:
         if len(term.string.split()) > 1:
             break
         multigram_index = term.index
+    sorted_list = terms.TermList([])
 
-    print(multigram_index)
-    sorted_list = terms.TermList()
+    for item in tqdm(terms_list.items[:multigram_index]):
+        next_sublist, nc = terms_list.return_related_items(item.string)
+        strings = next_sublist.get_strings()
+        for string in strings:
+            terms_list.get(string).order = 1
+        sorted_list = sorted_list + next_sublist
 
+    print("List has been sorted")
 
+    return sorted_list
 
 def main():
     #parser = init_argparser()
     #args = parser.parse_args()
     tsv_path = "energy_dataset/energy_terms.csv"
-    list_sorter(tsv_path)
+    terms_list, relevant_list = list_cleaner(tsv_path)
+    sorted_list = list_sorter(terms_list)
 
 
 if __name__ == "__main__":
