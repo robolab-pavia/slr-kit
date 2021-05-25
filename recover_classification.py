3207da728b74c0bf3c5ad8dd0415df8d65a0c103
dgiuffrida <gffdvd@gmail.com>
2021-05-25 12:30:09 +0200

    Changed default separator for output from ',' to '\t'

--- recover_classification.py
+++ recover_classification.py
@@ -13,9 +13,9 @@ import argparse
 def init_argparser():
     """Initialize the command line parser."""
     parser = argparse.ArgumentParser()
-    parser.add_argument('old_classification', action='store', type=str,
+    parser.add_argument('--old_classification', action='store', type=str,
                         help="old CSV data file partially classified")
-    parser.add_argument('new_classification', action='store', type=str,
+    parser.add_argument('--new_classification', action='store', type=str,
                         help="new CSV data file to be classified")
     parser.add_argument('--output', '-o', metavar='FILENAME', default='-',
                         help='output file name. If omitted or %(default)r '
@@ -29,12 +29,12 @@ def main():
     
     oc = pd.read_csv(args.old_classification, sep='\t',
                                         usecols=['term', 'label']).dropna(subset=['label'])
-    nc = nc1 = pd.read_csv(args.new_classification, sep='\t').dropna(subset=['term'])
+    nc = pd.read_csv(args.new_classification, sep='\t').dropna(subset=['term'])
     
     nc = nc.merge(oc, on='term', how='left')
     nc = nc.assign(label = nc['label_x'].combine_first(nc['label_y']))
     nc = nc.drop(['label_x', 'label_y'], axis=1)
-    nc.to_csv(args.output, index=False)
+    nc.to_csv(args.output, index=False, sep='\t')
 
 if __name__ == '__main__':
     main()
