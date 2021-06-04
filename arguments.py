f62915e9da7d057a37f8b43194b9c3a184f8f2ac
Guido Benetti <guido.benetti01@ateneopv.it>
2021-06-04 20:09:20 +0200

    Add custom ArgumentParser class to collect info about the scripts arguments

--- arguments.py
+++ arguments.py
@@ -109,3 +109,46 @@ class AppendMultiplePairsAction(SlrKitAction):
         pairs.append(pair)
 
         setattr(namespace, self.dest, pairs)
+
+
+class ArgParse(argparse.ArgumentParser):
+    def __new__(cls, *args, **kwargs):
+        obj = super().__new__(cls)
+        obj.slrkit_arguments = dict()
+        return obj
+
+    def add_argument(self, *name_or_flags, **kwargs):
+        non_standard = kwargs.pop('non_standard', False)
+        log = kwargs.pop('logfile', False)
+        action = kwargs.get('action', 'store')
+        ret = super().add_argument(*name_or_flags, **kwargs)
+        if action not in ['help', 'version']:
+            if ret.option_strings:
+                # take the longest option string without leading prefixes as the
+                # option name
+                options = [o.lstrip(self.prefix_chars) for o in ret.option_strings]
+                name = max(options, key=lambda o: len(o))
+            else:
+                name = ret.dest
+            if isinstance(ret, SlrKitAction):
+                default = ret.slrkit_conf_default
+            else:
+                default = ret.default
+                if ret.default is None:
+                    if action in ['append', 'extend']:
+                        default = []
+                    elif action == 'store_const':
+                        default = False
+                    elif action == 'count':
+                        default = 0
+
+            self.slrkit_arguments[name] = {
+                'value': default,
+                'help': ret.help % vars(ret),
+                'non-standard': non_standard,
+                'required': ret.required,
+                'dest': ret.dest,
+                'log': log,
+            }
+
+        return ret
