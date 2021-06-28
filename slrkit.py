f306946335f20fa974f632284bad93e04c0c0740
Guido Benetti <guido.benetti01@ateneopv.it>
2021-06-28 17:12:23 +0200

    Fix name mismatch for cli_only attribute of the custom argparse object

--- slrkit.py
+++ slrkit.py
@@ -158,7 +158,7 @@ def check_project(args, filename):
 def prepare_script_arguments(config, config_dir, confname, script_args):
     args = argparse.Namespace()
     for k, v in script_args.items():
-        if v.get('non-standard', False) or v.get('cli-only', False):
+        if v.get('non-standard', False) or v.get('cli_only', False):
             continue
         if v.get('logfile', False):
             setattr(args, k, str((config_dir / 'log' / 'slr-kit.log').resolve()))
