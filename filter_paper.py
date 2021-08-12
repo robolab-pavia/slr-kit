065b3f47f3e674a34a2aac83c449f1cdfeff6a88
Guido Benetti <guido.benetti01@ateneopv.it>
2021-08-12 14:49:54 +0200

    Add cli option to choose the column with the titles in abstracts file

--- filter_paper.py
+++ filter_paper.py
@@ -24,7 +24,10 @@ def init_argparser():
     parser.add_argument('journal_file', type=str,
                         help='path to the file with the classified journals',
                         input=True)
-
+    parser.add_argument('--title-column', '-t', action='store', type=str,
+                        default='title', dest='title_column',
+                        help='Column in abstract_file with the papers titles. '
+                             'If omitted %(default)r is used.')
     return parser
 
 
@@ -65,12 +68,14 @@ def ris_reader(ris_path):
     return pd.DataFrame(paper_journal_list)
 
 
-def append_label(abstracts, journal, paper_journal):
+def set_paper_status(abstracts, title_column, journal, paper_journal):
     """
     Labels every Paper from preproc list with the relative journal classification
 
     :param abstracts: DataFrame with every paper data
     :type abstracts: pd.DataFrame
+    :param title_column: name of the column with the papers titles
+    :type title_column: str
     :param journal: DataFrame with journal data and classification
     :param paper_journal: DataFrame with title-journal correspondence for every paper
     :type paper_journal: pd.DataFrame
@@ -79,8 +84,8 @@ def append_label(abstracts, journal, paper_journal):
     """
     good = journal[journal['label'].isin(['relevant', 'keyword'])]['term']
     pgood = paper_journal['journal'].apply(lambda t: any(good.isin([t])))
-    title_good = paper_journal[pgood]['title']
-    cond = abstracts['title'].apply(lambda t: any(title_good.isin([t])))
+    title_good = paper_journal[pgood][title_column]
+    cond = abstracts[title_column].apply(lambda t: any(title_good.isin([t])))
     abstracts['status'] = ''
     abstracts.loc[cond, 'status'] = 'good'
     abstracts.loc[~cond, 'status'] = 'rejected'
@@ -101,9 +106,9 @@ def filter_paper(args):
         msg = 'Error: file {!r} not found'
         sys.exit(msg.format(err.filename))
 
-    assert_column(str(abstracts_path), preproc, 'title')
+    assert_column(str(abstracts_path), preproc, args.title_column)
     assert_column(str(journal_path), journals, ['id', 'term', 'label'])
-    out = append_label(preproc, journals, paper_journal)
+    out = set_paper_status(preproc, args.title_column, journals, paper_journal)
     out.to_csv(abstracts_path, sep='\t', index=False)
 
 
