f8a47959b9c89fa75f065ac4eefcb260620c5954
Guido Benetti <guido.benetti01@ateneopv.it>
2021-09-08 18:08:46 +0200

    Refactor the script to move all the logic in a function that can be run from another script

--- join_lda_info.py
+++ join_lda_info.py
@@ -22,32 +22,30 @@ def init_argparser():
     return parser
 
 
-def main():
-    args = init_argparser().parse_args()
-    output = args.output
+def join_lda_info(loaded_topics, docs, output):
+    """
+    Joins the information about the topics assigned to each document and outputs them
 
-    with open(args.terms_topics) as topics_file:
-        loaded_topics = json.load(topics_file)
-
-
+    :param loaded_topics: information about the topics
+    :type loaded_topics: dict
+    :param docs: association between documents and topics
+    :type docs: list[dict]
+    :param output: output file. If None, stdout is used
+    :type output: str or None
+    """
     # for each topics keeps the title and the three highest terms
     topics = {}
     for p in loaded_topics:
         identifier = p
-        loaded_prob = loaded_topics[p]["terms_probability"]
+        loaded_prob = loaded_topics[p]['terms_probability']
         probab = []
         for pp in loaded_prob:
             probab.append((pp, loaded_prob[pp]))
         sorted_by_second = sorted(probab, key=lambda tup: tup[1], reverse=True)
         topics[identifier] = {
-                "name": loaded_topics[p]["name"],
-                "terms": sorted_by_second[:5]
+            'name': loaded_topics[p]['name'],
+            'terms': sorted_by_second[:5]
         }
-
-    #print(topics)
-
-    with open(args.docs_topics) as docs_file:
-        docs = json.load(docs_file)
 
     if output is not None:
         output_file = open(output, 'w')
@@ -66,6 +64,21 @@ def main():
             terms = [t[0] for t in topics[key]['terms']]
             print('  {:5.1f}% {} {}'.format(t[1] * 100, name, terms),
                   file=output_file)
+
+
+def main():
+    args = init_argparser().parse_args()
+    terms_topics = args.terms_topics
+    docs_topics = args.docs_topics
+    output = args.output
+
+    with open(terms_topics) as topics_file:
+        loaded_topics = json.load(topics_file)
+
+    with open(docs_topics) as docs_file:
+        docs = json.load(docs_file)
+
+    join_lda_info(loaded_topics, docs, output)
 
 
 if __name__ == '__main__':
